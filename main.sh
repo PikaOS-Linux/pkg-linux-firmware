213c7413ec644444518931be17f3bc6fb00785fc
ferrreo <harderthanfire@gmail.com>
2023-08-10 17:47:48 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,19 +2,19 @@
 
 # Clone Upstream
 #git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/ -b 20230515
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/ -b main
 cp -rvn ./debian-firmware/* ./linux-firmware/
 cd ./linux-firmware
 touch debian/changelog
 #echo -e "linux-firmware ("$(date '+%Y%m%d')".git-99pika"$(date '+%M')") lunar; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
-echo -e "linux-firmware (20230515.git-99pika"$(date '+%M')") lunar; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
+echo -e "linux-firmware (20230810.git-99pika"$(date '+%M')") lunar; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
 #LOGNAME=root dh_make --createorig -y -l -p linux-firmware_"$(date '+%Y%m%d')".git || echo "dh-make didn't go clean"
-LOGNAME=root dh_make --createorig -y -l -p linux-firmware_20230515.git || echo "dh-make didn't go clean"
+LOGNAME=root dh_make --createorig -y -l -p linux-firmware_20230810.git || echo "dh-make didn't go clean"
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
