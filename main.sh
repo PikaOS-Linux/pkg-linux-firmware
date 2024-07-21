bc56657376ec1781159de6c0e9d508f85db027af
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-22 00:28:13 +0300

    port to os 4

--- main.sh
+++ main.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-LC_ALL=C apt list --installed | grep -F '[installed,local]'
-
-exit 0
+VERSION=$(date '+%Y%m%d')
 
 # Clone Upstream
 #git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/
@@ -10,13 +8,13 @@ git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmwar
 cp -rvn ./debian-firmware/* ./linux-firmware/
 cd ./linux-firmware
 touch debian/changelog
-echo -e "linux-firmware ("$(date '+%Y%m%d')".git-100pika1) pikauwu; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
+echo -e "linux-firmware ("$VERSION".git-101pika1) canary; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p linux-firmware_"$(date '+%Y%m%d')".git || echo "dh-make didn't go clean"
+LOGNAME=root dh_make --createorig -y -l -p linux-firmware_"$VERSION".git || echo "dh-make didn't go clean"
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
